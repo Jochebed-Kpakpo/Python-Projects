3af0ef7cdfc6e565186cae8b3e19921f8b08ba94
Jochebed-Kpakpo <alloteyjochebed7@gmail.com>
2025-03-29 22:32:26 +0000

    Add shopping list project via upload
    Appends shopping list

---
+++ pf-l2-1.0/main.py
@@ -0,0 +1,16 @@
+shopping_list = []
+item1 = input("item 1")
+item2 = input("item 2")
+item3 = input("item 3")
+shopping_list = [item1, item2, item3]
+print(shopping_list)
+
+#Alternative
+shopping_list = []
+item1 = input("item 1")
+shopping_list.append(item1)
+item2 = input("item 2")
+shopping_list.append(item2)
+item3 = input("item 3")
+shopping_list.append(item3)
+print(shopping_list)
